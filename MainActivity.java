f34e71f7664f550a919e028de0dc3566c6cb205a
Phipson <phipsonleecy@gmail.com>
2017-04-01 23:03:41 -0700

    Add files via upload
    gson-2.8.0.jar saves data as a bundle for sharedpreferences in separate activities. okhttp-3.6.0.jar provides library for HTTP posting and internet access. Java Classes intend to update and store values from MySQL Database

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package ecomap.a2017.lahacks.ecomap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
